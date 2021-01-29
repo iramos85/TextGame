0cbffe492efafd602bdbe0eac836b11e077d7638
Ian Ramos <juventuddisponible@gmail.com>
2021-01-29 12:20:26 -0600

    added first state - startingState

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
 
-    Text textComponent;
+    [SerializeField] Text textComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = "This is a string.";
     }
 
     // Update is called once per frame
